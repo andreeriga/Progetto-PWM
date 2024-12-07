e1d8502279061261d4b5c37f8852bf0cfd191932
andrea.rigamonti11 <andrea.rigamonti11@studenti.unimi.it>
2024-12-08 00:09:03 +0100

    route per inserimento di un utente nel database

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { connectToDatabase } = require('./db');
+const { connectToDatabase, insertUser } = require('./db');
 
 //Carica le variabili d'ambiente
 require('dotenv').config();
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.get("/users", async (req, res) => {
@@ -15,10 +16,34 @@ app.get("/users", async (req, res) => {
         client = connection.client;
 
         const users = await db.collection("Users").find({}).toArray();
-        res.send(users);
+        res.status(200).send(users);
     } catch (err) {
         console.error("Errore nel recupero degli utenti:", err);
-        res.status(500).send("Errore interno del server");
+        res.status(500).send({message:"errore interno del server"});
+    } finally {
+        if (client) {
+            await client.close();
+            console.log("Connessione a MongoDB chiusa");
+        }
+    }
+});
+
+app.post("/users", async (req, res) => {
+    let client;
+    try {
+        const connection = await connectToDatabase();
+        const db = connection.db;
+        client = connection.client;
+        
+        const final_user = await insertUser(db, req.body);
+        res.status(200).send(final_user);
+    } catch (err) {
+        if (err.code == 11000) {
+            res.status(400).send({ message: "mail già registrata" });
+        } else {
+            console.error("Errore nel recupero degli utenti:", err);
+            res.status(500).send({ message: "errore interno del server" });
+        }
     } finally {
         if (client) {
             await client.close();
